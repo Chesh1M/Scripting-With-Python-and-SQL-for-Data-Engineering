da91b0a4dfd910386ab95d2319a790d1038833ea
Chesh1M <50264743+Chesh1M@users.noreply.github.com>
2024-01-02 15:47:20 +0000

    done with week 2

---
+++ Week 2 (Python Scripting and SQL)/week2_project.py
@@ -0,0 +1,66 @@
+'''
+Command line script to find the 10 largest files in the file system 
+based on the specified file extension (png, mp4, ipynb, py etc..)
+Uses in memory sqlite3 database and argparse
+'''
+
+import os, argparse, sqlite3
+
+## Create nested list to store paths and files
+files_and_size = []
+for root, dirs, files in os.walk('../../'):
+    for file in files:
+        tmp = []
+        # Get and append path to tmp
+        path = os.path.join(root, file)
+        tmp.append(path)
+        # Get and append size to tmp
+        size = os.path.getsize(path)
+        tmp.append(size)
+        # Append tmp as a list to files_and_size list 
+        files_and_size.append(tmp)
+
+
+## Connect and iniialize sqlite3 and table
+connection = sqlite3.connect(':memory:')
+table = 'CREATE TABLE files (id integer primary key, file_names TEXT, size INTEGER)'
+cursor = connection.cursor()
+cursor.execute(table)
+connection.commit()
+
+
+## Add files_and_size into the table created
+insert_query = 'INSERT INTO files (file_names, size) VALUES(?, ?)'
+for item in files_and_size:
+    cursor.execute(insert_query, item)
+connection.commit()
+
+
+## Query database to look for files matching the extension given
+def find_files_and_size(extension):
+    ext = '%' + extension
+    variable = []
+    variable.append(ext)
+    
+    query = '''
+    SELECT file_names, size
+    FROM files
+    WHERE file_names like ?
+    ORDER BY size desc
+    LIMIT 10
+    '''
+
+    for result in cursor.execute(query, variable):
+        print(result)
+
+
+## Defining argparser
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_extension", help="File extension (e.g. png/jpg etc)")
+    args = parser.parse_args()
+    return find_files_and_size(args.file_extension)
+
+
+if __name__ == '__main__':
+    main()
