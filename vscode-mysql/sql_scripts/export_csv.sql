c19876a545da0b861d1832e9f20736b2b15abd1d
Chesh1M <50264743+Chesh1M@users.noreply.github.com>
2024-01-10 01:26:29 +0800

    Delete vscode-mysql directory

--- vscode-mysql/sql_scripts/export_csv.sql
+++
@@ -1,8 +0,0 @@
-USE ratings;
-(SELECT 'name', 'rating', 'region' FROM ratings)
-UNION
-SELECT * FROM ratings
-INTO OUTFILE '../table.csv'
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
--- vscode-mysql/sql_scripts/query.sql
+++
@@ -1,2 +0,0 @@
-USE ratings;
-SELECT * from ratings LIMIT 100;
